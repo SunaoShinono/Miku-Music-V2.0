3874f255ad5d93464ee6e954540a73210aefed27
SunaoShinono <idnam3699@gmail.com>
2025-01-31 19:08:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -22,10 +22,10 @@ module.exports = {
       secure: false
     },
     {
-      name: "Catfein ID",
-      password: "catfein",
-      host: "lava.catfein.com",
-      port: 4000,
+      name: "Koi Node V4",
+      password: "prplmoe.me",
+      host: "lavav4.prplmoe.me",
+      port: 1118,
       secure: false
     },
     { 
