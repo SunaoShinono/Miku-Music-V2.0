ddf8b6c43b14b802336d41d196d430847652fbea
SunaoShinono <idnam3699@gmail.com>
2025-03-08 17:34:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,25 +2,46 @@
 
 module.exports = {
   TOKEN: "",
-  language: "en",
-  ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
-  spotifyClientId : "",
-  spotifyClientSecret : "",
+  language: "th",
+  ownerID: ["705260973669482507", ""], 
+  mongodbUri : "mongodb+srv://Shinono:Sunao_Shinono@miku-bot.pbceb.mongodb.net/?retryWrites=true&w=majority&appName=Miku-Bot",
+  spotifyClientId : "bddc81fcd0af46d9b05a278f8f9f9939",
+  spotifyClientSecret : "ee11fa4a59bf4f64b5a6433839b49c02",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
-  activityName: "YouTube Music", 
+  activityName: "กำลังรอขึ้นเวที ใช้คำสั่ง /play", 
   activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
   SupportServer: "https://discord.gg/xQF9f9yUEM",
-  embedTimeout: 5, 
+  embedTimeout: 10, 
   errorLog: "", 
   nodes: [
-     {
-      name: "XX",
-      password: "enteryourcustompass",
-      host: "180.188.226.76",
-      port:  7019,
+   {
+      name: "INZEWORLD.COM (DE)",
+      password: "saher.inzeworld.com",
+      host: "lava.inzeworld.com",
+      port: 3128,
+      secure: false
+    },
+    {
+      name: "ChalresNaig Node",
+      password: "NAIGLAVA-dash.techbyte.host",
+      host: "lavahatry4.techbyte.host",
+      port: 3000,
+      secure: false
+    },  
+    {
+      name: "Public Lavalink v4 NonSSL",
+      password: "https://dsc.gg/ajidevserver",
+      host: "lava-v4.ajieblogs.eu.org",
+      port: 80,
+      secure: false
+    },
+        {
+      name: "jirayu",
+      password: "youshallnotpass",
+      host: "lavalink.jirayu.net",
+      port: 13592,
       secure: false
     }
   ]
