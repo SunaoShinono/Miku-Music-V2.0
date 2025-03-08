3fa10b695621d3218123bb2dcc5538bce8533622
SunaoShinono <idnam3699@gmail.com>
2025-03-08 17:28:38 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,27 @@
+
+
+module.exports = {
+  TOKEN: "",
+  language: "en",
+  ownerID: ["1004206704994566164", ""], 
+  mongodbUri : "",
+  spotifyClientId : "",
+  spotifyClientSecret : "",
+  setupFilePath: './commands/setup.json',
+  commandsDir: './commands',  
+  embedColor: "#1db954",
+  activityName: "YouTube Music", 
+  activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
+  SupportServer: "https://discord.gg/xQF9f9yUEM",
+  embedTimeout: 5, 
+  errorLog: "", 
+  nodes: [
+     {
+      name: "XX",
+      password: "enteryourcustompass",
+      host: "180.188.226.76",
+      port:  7019,
+      secure: false
+    }
+  ]
+}
