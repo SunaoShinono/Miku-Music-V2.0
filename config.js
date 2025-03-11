4628e45bc7f16dfc5a77d63330eda8809c729e23
SunaoShinono <idnam3699@gmail.com>
2025-03-11 14:52:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -17,31 +17,10 @@ module.exports = {
   errorLog: "", 
   nodes: [
     {
-      name: "INZEWORLD.COM (DE)",
-      password: "saher.inzeworld.com",
-      host: "lava.inzeworld.com",
-      port: 3128,
-      secure: false
-    },
-    {
-      name: "ChalresNaig Node",
-      password: "NAIGLAVA-dash.techbyte.host",
-      host: "lavahatry4.techbyte.host",
-      port: 3000,
-      secure: false
-    },  
-    {
-      name: "Public Lavalink v4 NonSSL",
-      password: "https://dsc.gg/ajidevserver",
-      host: "lava-v4.ajieblogs.eu.org",
-      port: 80,
-      secure: false
-    },
-        {
-      name: "jirayu",
-      password: "youshallnotpass",
-      host: "lavalink.jirayu.net",
-      port: 13592,
+      name: "Hatsune Miku Node Server",
+      password: "hatsune_miku",
+      host: "78.46.65.243",
+      port: 5979,
       secure: false
     }
   ]
