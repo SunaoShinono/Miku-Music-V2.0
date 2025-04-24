6b73b15871e3192a793598c28157d1844b134008
SunaoShinono <idnam3699@gmail.com>
2025-04-24 10:00:17 +0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -6,7 +6,7 @@ const musicIcons = require('../UI/icons/musicicons.js');
 
 module.exports = {
   name: "help",
-  description: "Get information about the bot",
+  description: "ดูข้อมูลเกี่ยวกับบอทและคำสั่งที่สามารถใช้งานได้",
   permissions: "0x0000000000000800",
   options: [],
   run: async (client, interaction, lang) => {
