1aefef9de1de541b016eb073d684ec71b61849d6
SunaoShinono <idnam3699@gmail.com>
2025-04-25 14:32:38 +0700

    Update stop.js

--- commands/stop.js
+++ commands/stop.js
@@ -54,7 +54,7 @@ async function stop(client, interaction, lang) {
 
 module.exports = {
     name: "stop",
-    description: "Stop the current song and destroy the player",
+    description: "หยุดเล่นเพลงทั้งหมดและออกจากห้องเสียง",
     permissions: "0x0000000000000800",
     options: [],
     run: stop
