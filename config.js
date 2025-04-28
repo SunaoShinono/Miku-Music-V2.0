f53bfa77660b10e84eaf73a5c7f6a12f7c62504f
SunaoShinono <idnam3699@gmail.com>
2025-04-28 13:03:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -22,13 +22,13 @@ module.exports = {
 	host: "lava-v4.ajieblogs.eu.org",
 	port: 80,
 	secure: false
+},
+{
+	name: "Hatsune Miku",
+	password: "hatsune_miku",
+	host: "sudo.pylex.xyz",
+	port: 11568,
+	secure: false
 }
-//{
-//	name: "Hatsune Miku",
-//	password: "hatsune_miku",
-//	host: "sudo.pylex.xyz",
-//	port: 11568,
-//	secure: false
-//}
   ]
 }
